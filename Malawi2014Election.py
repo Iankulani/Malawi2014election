71477661f9c68be58e0ea24ff5e09b5b19c77f1d
Ian Carter Kulani <161088906+Iankulani@users.noreply.github.com>
2024-03-02 23:22:53 +0200

    Update Malawi2014Election.py
    This software prompts the user to enter total number of published centers, total number of registered voters, total number of null_&_ void votes, total number of valid votes and total valid votes for each candidate. Afterward, it determines the candidate with the majority of votes and displays the results on the screen.
    NOTE: For a candidate to be a majority winner the candidate total valid votes should be greater than majority votes.

--- Malawi2014Election.py
+++ Malawi2014Election.py
@@ -4,19 +4,18 @@ Created on Sat Feb 17 02:31:52 2024
 
 @author: IAN CARTER KULANI
 """
-
-#This software prompts the user to enter total number of published centers, total number of registered votes, total number of null and void votes, total number of valid votes and total valid votes for each candidate. Afterward, it determines the candidate with the majority of votes and displays the results on the screen.
+#This software prompts the user to enter total number of published centers, total number of registered voters, total number of null and void votes, total number of valid votes and total valid votes for each candidate. Afterward, it determines the candidate with the majority of votes and displays the results on the screen.
 #NOTE:For a candidate to be a majority winner the candidate total valid votes should be greater than majority votes.
 
 print("==============================MALAWI ELECTROL COMMISSION==============================\n") 
 
 Totalpublishedcenters=int(input("Enter Total published centers:"))
 #Get the total number of registered voters
-TotalRegisteredvotes=int(input("Enter Total Registered Voters:"))
+TotalRegisteredvotes=int(input("Enter Total Registered Voters/Turnout:"))
 #Get Total number of votes cast
-Totalvotescast=int(input("Enter Total Votes Cast:"))
+Totalvotescast=int(input("Enter Total Votes Cast/Total Votes:"))
 #Get total number of Null_&_Void votes
-Nullandvoid=int(input("Enter Total Null and Void Votes:"))
+Nullandvoid=int(input("Enter Total Null_&_Void Votes/Invalid Votes:"))
 #Get Total Valid Votes for All candidates
 Totalvalidvotes=int(input("Enter Total Valid Votes:"))
 #Get Total Valid Votes for Peter Wa Mutharika
@@ -85,33 +84,58 @@ elif Totalvalidvotesfor_Daviskatsonga>Totalvalidvotes/2+1:
 else:
     print("No majority winner was found RUNOF may be required Thank you.\n\n")
 
-#Calculating Majority
+
 
 print("_________________________________ELECTION STATISTICS_____________________________________\n")
-majority_percent=Totavalidvotesfor_Petermutharika*percent/Totalvalidvotes;
-print("Total Valid Votes for Peter Wa Mutharika in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Lazaruschawera*percent/Totalvalidvotes;
-print("Total Valid Votes for Dr Lazarus MacCarthy Chakwera in percentage=",majority_percent)
-majority_percent=Totavalidvotesfor_Joycebanda*percent/Totalvalidvotes;
-print("Total Valid Votes for Joyce Banda in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Atupelemuluzi*percent/Totalvalidvotes;
-print("Total Valid Votes for Atupele Muluzi in percentage=", majority_percent)
-majority_percent=Totavalidvotesfor_Kamuzuchibambo*percent/Totalvalidvotes;
-print("Total Valid Votes for Kamuzu Chibambo in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Markkatsonga*percent/Totalvalidvotes;
-print("Total Valid Votes for Mark Katsongain percentage=",majority_percent)
-majority_percent=Totavalidvotesfor_Johnchisi*percent/Totalvalidvotes;
-print("Total Valid Votes for John Chisi in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Georgennesa*percent/Totalvalidvotes;
-print("Total Valid Votes for George Nnesa in percentage=", majority_percent)
-majority_percent=Totavalidvotesfor_Jamesnyondo*percent/Totalvalidvotes;
-print("Total Valid Votes for James Nyondo in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Hellensighn*percent/Totalvalidvotes;
-print("Total Valid Votes for Hellen Sighn in percentage=",majority_percent)
-majority_percent=Totavalidvotesfor_Fridayjumbe*percent/Totalvalidvotes;
-print("Total Valid Votes for Friday Jumbe in percentage=",majority_percent)
-majority_percent=Totalvalidvotesfor_Daviskatsonga*percent/Totalvalidvotes;
-print("Total Valid Votes for Davis Katsonga in percentage=",majority_percent)
+#Calculating percentage
+#Calculating percentage for total votes cast
+Percentage=round(Totalvalidvotes*percent/Totalvalidvotes, 2);
+print("Total Votes Cast in percentage=",Percentage)
+#Calculating percentage for total valid votes for all canidates
+Percentage=round(Totalvalidvotes*percent/Totalvotescast, 2);
+print("Total Valid Votes for all candidtes in percentage=",Percentage)
+#Calculating percentage for Total Registered voters/turnout 
+Percentage=round(Totalvotescast*percent/TotalRegisteredvotes, 2);
+print("Total Registered voters/turnout in percentage=",Percentage)
+#Calculating percentage for null_&_void votes
+Percentage=round(Nullandvoid*percent/Totalvalidvotes, 2);
+print("Total Null_&_Void in percentage=",Percentage)
+#Calculating percentage for Peter Wa Mutharika
+Percentage=round(Totavalidvotesfor_Petermutharika*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Peter Wa Mutharika in percentage=",Percentage)
+#Calculating percentage for Dr Lazarus MacCarthy Chakwera
+Percentage=round(Totalvalidvotesfor_Lazaruschawera*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Dr Lazarus MacCarthy Chakwera in percentage=",Percentage)
+#Calculating percentage for Joyce Banda
+Percentage=round(Totavalidvotesfor_Joycebanda*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Joyce Banda in percentage=",Percentage)
+#Calculating percentage for Atupele Muluzi
+Percentage=round(Totalvalidvotesfor_Atupelemuluzi*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Atupele Muluzi in percentage=",Percentage)
+#Calculating percentage for Kamuzu Chibambo
+Percentage=round(Totavalidvotesfor_Kamuzuchibambo*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Kamuzu Chibambo in percentage=",Percentage)
+#Calculating percentage for Mark Katsonga
+Percentage=round(Totalvalidvotesfor_Markkatsonga*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Mark Katsonga in percentage=",Percentage)
+#Calculating percentage for John Chisi
+Percentage=round(Totavalidvotesfor_Johnchisi*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for John Chisi in percentage=",Percentage)
+#Calculating percentage for George Nnesa
+Percentage=round(Totalvalidvotesfor_Georgennesa*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for George Nnesa in percentage=",Percentage)
+#Calculating percentage for James Nyondo
+Percentage=round(Totavalidvotesfor_Jamesnyondo*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for James Nyondo in percentage=",Percentage)
+#Calculating percentage for Hellen Sighn 
+Percentage=round(Totalvalidvotesfor_Hellensighn*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Hellen Sighn in percentage=",Percentage)
+#Calculating percentage for Friday Jumbe
+Percentage=round(Totavalidvotesfor_Fridayjumbe*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Friday Jumbe in percentage=",Percentage)
+#Calculating percentage for Davis Katsonga 
+Percentage=round(Totalvalidvotesfor_Daviskatsonga*percent/Totalvalidvotes, 2);
+print("Total Valid Votes for Davis Katsonga in percentage=",Percentage)
 
 
 print("==========================================================================================\n") 
